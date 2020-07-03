f0ed341d2d9ed8db71e6772dea38a838132c051f
Ariel Montoya <ariel_montoya15@outlook.es>
2020-07-02 23:39:31 -0500

    b

--- ProyectoTB.vhd
+++ ProyectoTB.vhd
@@ -2,15 +2,15 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   22:55:44 11/24/2019
+-- Create Date:   23:18:09 07/02/2020
 -- Design Name:   
--- Module Name:   C:/Users/reyja/Downloads/ProyectoESCOMipsPV/ProyectoTB.vhd
--- Project Name:  ProyectoESCOMipsPV
+-- Module Name:   C:/Users/DANIEL/Desktop/Ilusion/ilusion/ProyectoTB.vhd
+-- Project Name:  ilusion
 -- Target Device:  
 -- Tool versions:  
 -- Description:   
 -- 
--- VHDL Test Bench Created by ISE for module: Proyecto
+-- VHDL Test Bench Created by ISE for module: ProyectoESCOMipsPV
 -- 
 -- Dependencies:
 -- 
@@ -39,57 +39,57 @@ ARCHITECTURE behavior OF ProyectoTB IS
  
     -- Component Declaration for the Unit Under Test (UUT)
  
-    COMPONENT Proyecto
+    COMPONENT ProyectoESCOMipsPV
     PORT(
-         CLR : IN  std_logic;
-         CLK : IN  std_logic;
+         RCLR : IN  std_logic;
+         RCLK : IN  std_logic;
          salida : OUT  std_logic_vector(7 downto 0);
-         PC : OUT  std_logic_vector(7 downto 0)
+         contProg : OUT  std_logic_vector(7 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
-   signal CLR : std_logic := '0';
-   signal CLK : std_logic := '0';
+   signal RCLR : std_logic := '0';
+   signal RCLK : std_logic := '0';
 
- 	--Outputs
+  --Outputs
    signal salida : std_logic_vector(7 downto 0);
-   signal PC : std_logic_vector(7 downto 0);
+   signal contProg : std_logic_vector(7 downto 0);
 
    -- Clock period definitions
-   constant CLK_period : time := 10 ns;
+   constant RCLK_period : time := 10 ns;
  
 BEGIN
  
-	-- Instantiate the Unit Under Test (UUT)
-   uut: Proyecto PORT MAP (
-          CLR => CLR,
-          CLK => CLK,
+  -- Instantiate the Unit Under Test (UUT)
+   uut: ProyectoESCOMipsPV PORT MAP (
+          RCLR => RCLR,
+          RCLK => RCLK,
           salida => salida,
-          PC => PC
+          contProg => contProg
         );
 
    -- Clock process definitions
-   CLK_process :process
+   RCLK_process :process
    begin
-		CLK <= '0';
-		wait for CLK_period/2;
-		CLK <= '1';
-		wait for CLK_period/2;
+    RCLK <= '0';
+    wait for RCLK_period/2;
+    RCLK <= '1';
+    wait for RCLK_period/2;
    end process;
  
+
 
    -- Stimulus process
    stim_proc: process
    begin
-		clr<='1';
-		wait for 12ns;
---		wait until falling_edge(clk);
---		wait until falling_edge(clk);
-		clr<='0';
+    RCLR<='1';
+    wait for 12ns;
+--    wait until falling_edge(clk);
+--    wait until falling_edge(clk);
+    RCLR<='0';
 
       wait;
    end process;
-
 END;
